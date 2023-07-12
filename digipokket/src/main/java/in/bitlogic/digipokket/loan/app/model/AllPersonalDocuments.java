44cc17d972e253b757102ed7dbf419f22086a850
GSBhosale <gauravsanjaybhosale@gmail.com>
2023-07-12 16:25:33 +0530

    POJO-- AllPersonalDocuments created.

---
+++ digipokket/src/main/java/in/bitlogic/digipokket/loan/app/model/AllPersonalDocuments.java
@@ -0,0 +1,38 @@
+package in.bitlogic.digipokket.loan.app.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Lob;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class AllPersonalDocuments {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Integer docsId;
+	@Lob
+	private Byte[] addressProof;
+	@Lob
+	private Byte[] panCard;
+	@Lob
+	private Byte[] adharCard;
+	@Lob
+	private Byte[] incomeProof;
+	@Lob
+	private Byte[] bankPassbook;
+	@Lob
+	private Byte[] passportPhoto;
+	@Lob
+	private Byte[] signature;
+	@Lob
+	private Byte[] thumb;
+}
