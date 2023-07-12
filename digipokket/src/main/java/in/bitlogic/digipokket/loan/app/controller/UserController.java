1453ffceb64935e533138670690b364229ed1d7c
GSBhosale <gauravsanjaybhosale@gmail.com>
2023-07-13 01:40:51 +0530

    PUSH___till authenticating user(Login)

--- digipokket/src/main/java/in/bitlogic/digipokket/loan/app/controller/UserController.java
+++ digipokket/src/main/java/in/bitlogic/digipokket/loan/app/controller/UserController.java
@@ -1,7 +1,6 @@
 package in.bitlogic.digipokket.loan.app.controller;
 
 import java.io.IOException;
-import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -9,8 +8,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -19,7 +16,7 @@ import org.springframework.web.multipart.MultipartFile;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
-
+import in.bitlogic.digipokket.loan.app.model.Enquiry;
 import in.bitlogic.digipokket.loan.app.model.User;
 import in.bitlogic.digipokket.loan.app.service.UserService;
 
@@ -44,23 +41,6 @@ public class UserController {
 		
 		return new ResponseEntity<User>(user,HttpStatus.CREATED);
 	}
-	
-	@GetMapping("/getUser")
-	public ResponseEntity<List<User>> getUser()
-	{
-		List<User> liuser=userService.getUser();
-		return new ResponseEntity<List<User>>(liuser,HttpStatus.OK);
-		
-
-	}
-	@PutMapping("/updateUser/{userId}")
-	public User updateUser(@PathVariable int userId,@RequestBody User u)
-	{
-		
-		u.setUserId(userId);
-		System.out.println(u.getUserId());
-		return userService.updateUser(u);
-	}
 	@GetMapping("/authenticateUser/{username}/{password}")
 	public ResponseEntity<User> authenticateUser(@PathVariable("username") String username,@PathVariable("password") String password)
 	{
@@ -69,7 +49,7 @@ public class UserController {
 		{
 			return new ResponseEntity<User>(user,HttpStatus.OK);
 		}
-		 return null;
+		return null;
 	}
 
 }
