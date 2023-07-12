9c885909d557e6e74929019e1b324b4c831ec2c7
GSBhosale <gauravsanjaybhosale@gmail.com>
2023-07-12 15:07:50 +0530

    project created successfully..!!

---
+++ digipokket/src/main/java/in/bitlogic/digipokket/app/DigipokketApplication.java
@@ -0,0 +1,13 @@
+package in.bitlogic.digipokket.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigipokketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DigipokketApplication.class, args);
+	}
+
+}
---
+++ digipokket/src/test/java/in/bitlogic/digipokket/app/DigipokketApplicationTests.java
@@ -0,0 +1,13 @@
+package in.bitlogic.digipokket.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigipokketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
