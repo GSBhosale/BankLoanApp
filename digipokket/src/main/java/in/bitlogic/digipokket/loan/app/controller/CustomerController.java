fc2aa8e0418a2b84c269b4432d24c6980cb5e658
GSBhosale <gauravsanjaybhosale@gmail.com>
2023-07-13 01:57:38 +0530

    CRUD--Customer--folder Stucture

---
+++ digipokket/src/main/java/in/bitlogic/digipokket/loan/app/controller/CustomerController.java
@@ -0,0 +1,10 @@
+package in.bitlogic.digipokket.loan.app.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/customer")
+public class CustomerController {
+
+}
