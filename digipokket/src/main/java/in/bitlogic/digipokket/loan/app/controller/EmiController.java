ed3640e47de5b7ea583340ce6bcd55fd03141e3b
rahul7738813312 <rahulfegade586@gmail.com>
2023-07-20 16:23:58 +0530

    EmiCalculator

--- digipokket/src/main/java/in/bitlogic/digipokket/loan/app/controller/EmiController.java
+++ digipokket/src/main/java/in/bitlogic/digipokket/loan/app/controller/EmiController.java
@@ -1,37 +1,44 @@
 package in.bitlogic.digipokket.loan.app.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import in.bitlogic.digipokket.loan.app.model.EmiCalculator;
 
+@CrossOrigin("*")
 @RestController
 @RequestMapping("/emiCalculator")
 public class EmiController {
 	
-//	@Autowired
-//	private EmiCalculator e;
-//	
-//	@PostMapping("/emical")
-//	public EmiCalculator emical(@RequestParam("requriedAmount") double am,@RequestParam("rateOIintreast") double r,@RequestParam("tenour") int n )
-//	{
-//		e.setRequriedAmount(am);
-//
-//		e.setRateOIintreast(r);
-//		e.setTenour(n);
-//		r=r/(12*100);
-//		n=n*12;
-//		 double E=am*r*(Math.pow(1+r,n)/(Math.pow(1+r,n)-1));
-//		 e.setEmi(E);
-//		 double totalAm=E*n;
-//		 e.setTotalAmount(totalAm);
-//		 double totalInterest=totalAm-am;
-//		 e.setTotalIntreast(totalInterest);
-//		return e;
-//		
-//	}
+	
+	
+	@PostMapping("/emical")
+	public EmiCalculator emical(@RequestBody EmiCalculator e)
+	{
+		//e.setRequriedAmount(am);
+		double r=e.getRateOIintreast();
+		
+		//e.setRateOIintreast(r);
+		int n=e.getTenour();
+		
+		//e.setTenour(n);
+		
+		double am=e.getRequriedAmount();
+		r=r/(12*100);
+		n=n*12;
+		 double E=am*r*(Math.pow(1+r,n)/(Math.pow(1+r,n)-1));
+		 e.setEmi(E);
+		 double totalAm=E*n;
+		 e.setTotalAmount(totalAm);
+		 double totalInterest=totalAm-am;
+		 e.setTotalIntreast(totalInterest);
+		return e;
+		
+	}
 
 }
