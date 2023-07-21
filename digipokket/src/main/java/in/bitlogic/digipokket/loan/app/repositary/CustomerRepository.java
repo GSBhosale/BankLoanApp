5fdd05c5c73acf743a21691222bb82a09c6ca7f8
vikasJawale <vikasjawale10@gmail.com>
2023-07-21 16:56:28 +0530

    CRUD--get sanctioned

--- digipokket/src/main/java/in/bitlogic/digipokket/loan/app/repositary/CustomerRepository.java
+++ digipokket/src/main/java/in/bitlogic/digipokket/loan/app/repositary/CustomerRepository.java
@@ -1,5 +1,7 @@
 package in.bitlogic.digipokket.loan.app.repositary;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,9 @@ import in.bitlogic.digipokket.loan.app.model.Customer;
 
 @Repository
 public interface CustomerRepository extends JpaRepository<Customer, Integer>{
+
+	public List<Customer> findByApplicationStatus(String valueOf);
+
+	
 
 }
--- digipokket/src/main/java/in/bitlogic/digipokket/loan/app/service/SanctionService.java
+++ digipokket/src/main/java/in/bitlogic/digipokket/loan/app/service/SanctionService.java
@@ -1,7 +1,9 @@
 package in.bitlogic.digipokket.loan.app.service;
 
 import java.io.ByteArrayInputStream;
+import java.util.List;
 
+import in.bitlogic.digipokket.loan.app.model.Customer;
 import in.bitlogic.digipokket.loan.app.model.SanctionLetter;
 
 public interface SanctionService 
@@ -10,5 +12,12 @@ public interface SanctionService
 	public ByteArrayInputStream createPdf(int customerId);
 
 	public SanctionLetter sanctionLoan(SanctionLetter sanLetter, int customerId);
+
+	public List<Customer> getSanction(String valueOf);
+
+
+	
+
+
 
 }
